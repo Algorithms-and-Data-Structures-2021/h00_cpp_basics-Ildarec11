60b996271c1046c789d2c0309a350eb69335f873
IldarArsl <arslambekov63@gmail.com>
2021-03-03 18:54:30 +0300

    remove mistakes

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -13,16 +13,15 @@ using std::copy;
 void swap_args(int *lhs, int *rhs) {
     // напишите код здесь ...
     if (lhs!= nullptr & rhs!= nullptr) {
-        int x = *lhs;
+        int temp = *lhs;
         *lhs = *rhs;
-        *rhs = x;
+        *rhs = temp;
     }
 }
 
 // Задание 2
 int **allocate_2d_array(int num_rows, int num_cols, int init_value) {
     // напишите код здесь ...
-
     if (num_rows<=0 | num_cols<=0) {
         return nullptr;
     }
@@ -44,38 +43,33 @@ bool copy_2d_array(int **arr_2d_source, int **arr_2d_target, int num_rows, int n
     // напишите код здесь ...
     if (arr_2d_source == nullptr | arr_2d_target == nullptr | num_rows <=0 | num_cols <=0)  {
         return false;
-    } else {
+    }
         for (int i=0; i<num_rows; i++) {
             for (int j=0; j<num_cols; j++) {
                 arr_2d_target[i][j] = arr_2d_source[i][j];
             }
         }
         return true;
-    }
 }
 
 // Задание 4
 void reverse_1d_array(vector<int> &arr) {
     // напишите код здесь ...
-    if (arr.size() == 0) {
-    } else {
-            for (int i = 0; i < arr.size() / 2; i++) {
-                int x = arr[i];
-                arr[i] = arr[arr.size() - i-1];
-                arr[arr.size() - i-1] = x;
-            }
+    if (arr.size() > 0) {
+        for (int i = 0; i < arr.size() / 2; i++) {
+            int x = arr[i];
+            arr[i] = arr[arr.size() - i-1];
+            arr[arr.size() - i-1] = x;
+        }
     }
 }
 
 // Задание 5
 void reverse_1d_array(int *arr_begin, int *arr_end) {
     // напишите код здесь ...
-    if (arr_begin == nullptr | arr_end == nullptr) {
-    } else {
-        while (arr_end - arr_begin!=1 | arr_end - arr_begin != 2) {
-            int x = *arr_begin;
-            *arr_begin = *arr_end;
-            *arr_end = x;
+    if (arr_begin != nullptr & arr_end != nullptr ) {
+        while (arr_end > arr_begin) {
+            swap_args(arr_begin, arr_end);
             arr_end--;
             arr_begin++;
         }
@@ -85,9 +79,7 @@ void reverse_1d_array(int *arr_begin, int *arr_end) {
 // Задание 6
 int *find_max_element(int *arr, int size) {
     // напишите код здесь ...
-    if (arr == nullptr | size <=0) {
-        return nullptr;
-    } else {
+    if (arr != nullptr & size > 0) {
         int *maxElement = arr;
         for (int i = 0; i < size; i++) {
             if (arr[i] > *maxElement) {
@@ -96,6 +88,7 @@ int *find_max_element(int *arr, int size) {
         }
         return maxElement;
     }
+    return nullptr;
 }
 
 // Задание 7
